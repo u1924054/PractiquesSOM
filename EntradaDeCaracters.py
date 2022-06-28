d5a8b437918497adec5733ddeeb7bbeac615cde3
Dalila <u1924054@campus.udg.edu>
2022-06-28 12:23:44 +0200

    Nou fitxer modificat dintre de la meva branca

--- EntradaDeCaracters.py
+++ EntradaDeCaracters.py
@@ -2,3 +2,5 @@ name = input('Entra el teu nom: ')
 edat = int(input ('Entra la teva edat: '))
 año100 = (100-edat)+2022
 print("En el año " + str(año100) + ' cumpliras 100 años')
+# linia para probar el funcionamiento de commit en mi nueva rama
+print('Hola hola caracola')

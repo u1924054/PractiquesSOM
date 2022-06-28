5d604a74f605c9af57cefdb74688acba54c70d24
Dalila <u1924054@campus.udg.edu>
2022-06-28 13:01:12 +0200

    Nom variable canviat

--- EntradaDeCaracters.py
+++ EntradaDeCaracters.py
@@ -1,6 +1,6 @@
 name = input('Entra el teu nom: ')
 edat = int(input ('Entra la teva edat: '))
-año100 = (100-edat)+2022
-print("En el año " + str(año100) + ' cumpliras 100 años')
+any100 = (100-edat)+2022
+print("En el año " + str(any100) + ' cumpliras 100 años')
 # linia para probar el funcionamiento de commit en mi nueva rama
 print('Hola hola caracola')
